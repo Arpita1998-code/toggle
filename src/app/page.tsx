48e86fc22a851c498ca60d69ead7d800a3e8859d
Arpita Dutta <arpitadutta808@gmail.com>
2024-03-14 19:26:33 +0530

    toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,93 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
+const Page = () => {
+  const [toggleChecked, setToggleChecked] = useState(() => Array(7).fill(false));
+  const [isChecked, setIsChecked] = useState(() => {
+    return Array.from({ length: 7 }, () => Array(5).fill(false));
+  });
+
+  const handleRowToggle = (rowIndex: number) => {
+    const updatedToggleState = [...toggleChecked];
+    updatedToggleState[rowIndex] = !toggleChecked[rowIndex];
+    setToggleChecked(updatedToggleState);
+  };
+
+  const handleToggleClick = (rowIndex: number, colIndex: number) => {
+    if (toggleChecked[rowIndex]) {
+      const updatedCheckedState = [...isChecked];
+      updatedCheckedState[rowIndex][colIndex] = !isChecked[rowIndex][colIndex];
+      setIsChecked(updatedCheckedState);
+    }
+  };
+
+  const handleToggleAll = () => {
+    const allUnchecked = !toggleChecked.every(row => row);
+    setToggleChecked(Array(7).fill(allUnchecked));
+    if (allUnchecked) {
+      setIsChecked(Array.from({ length: 7 }, () => Array(5).fill(false)));
+    }
+  };  
+
+  useEffect(() => {
+    isChecked.forEach((row, index) => {
+      const permissions = ['All', 'Read', 'Write', 'Create', 'Delete'];
+      const permissionsString = permissions.map((permission, i) => `${permission}: ${isChecked[index][i]}`).join(', ');
+      console.log(`${String.fromCharCode(65 + index)} -> ${permissionsString}`);
+    });
+  }, [isChecked]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center items-center h-screen">
+      <form className="grid grid-cols-0 gap-2 w-3/4">
+      <div className="grid grid-cols-5 gap-4 mb-4">
+          <label htmlFor="check-all" className="flex items-center font-bold">
+            <input type="checkbox" id="check-all" checked={toggleChecked.every(row => row)} onChange={handleToggleAll} />
+            <span>Toggle All</span>
+          </label>
+          <h1 className='font-bold absolute top-6 left-[330px]'>Active Permissions</h1>
+          <h1 className='font-bold absolute top-6 left-[885px]'>All</h1>
+          <h1 className='font-bold absolute top-6 left-[965px]'>Read</h1>
+          <h1 className='font-bold absolute top-6 left-[1060px]'>Write</h1>
+          <h1 className='font-bold absolute top-6 left-[1150px]'>Create</h1>
+          <h1 className='font-bold absolute top-6 left-[1240px]'>Delete</h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {Array.from(Array(7), (_, rowIndex) => (
+          <div key={rowIndex} className="bg-gray-200 rounded-md p-4 text-center border border-gray-400 relative" style={{ height: '80px' }}>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <label htmlFor={`check-all-${rowIndex}`} className="flex bg-gray-500 cursor-pointer relative left-[10px] top-[-20px] w-10 h-5 rounded-full">
+                  <input
+                    type="checkbox"
+                    id={`check-all-${rowIndex}`}
+                    className="sr-only peer"
+                    checked={toggleChecked[rowIndex]}
+                    onChange={() => handleRowToggle(rowIndex)}
+                  />
+                  <span className={`w-2/5 h-4/5 bg-gray-300 absolute rounded-full left-1 top-1 peer checked:bg-gray-600 peer-checked:left-5 transition-all duration-500 ${toggleChecked[rowIndex] ? 'bg-blue-500' : 'bg-gray-400'}`}></span>
+                </label>
+                <h1>{String.fromCharCode(65 + rowIndex)}</h1>
+              </div>
+              <div className="flex items-center space-x-20 relative top-[-20px] left-[-20px] p-8">
+              {Array.from(Array(5), (_, colIndex) => (
+                <label key={colIndex} className={`switch ${!toggleChecked[rowIndex] ? 'disabled' : ''}`}>
+                    <input
+                    type="checkbox"
+                    id={`check-${rowIndex}-${colIndex}`}
+                    checked={isChecked[rowIndex][colIndex]}
+                    onChange={() => handleToggleClick(rowIndex, colIndex)}
+                    disabled={!toggleChecked[rowIndex]}
+                    />
+                  <span className={`slider ${isChecked[rowIndex][colIndex] ? 'bg-blue-500' : 'bg-gray-400'}`}></span>
+                </label>
+              ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </form>
+    </div>
   );
-}
+};
+
+export default Page;
